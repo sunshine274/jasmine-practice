8d0d536b941dd303d1d8fb861eee80629e6886d5
sunshine274 <jingx274@gmail.com>
2020-06-25 08:52:44 +0000

    Initiate project

---
+++ spec/calcSpec.js
@@ -0,0 +1,11 @@
+
+describe("Calculator", function(){
+    describe("Addition function", function(){
+        it("should return 42", function(){
+            expect(addition(20,22)).toBe(42);
+        });
+        it("should return 26", function(){
+            expect( addition(7,19)).toBe(26);
+        });
+    });
+});
