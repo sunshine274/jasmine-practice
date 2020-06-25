84f7b3c8b65a0844aa71b866e6a40fa6f8a1ab71
sunshine274 <jingx274@gmail.com>
2020-06-25 10:47:14 +0000

    Added practice code

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,28 @@
-function addition(numOne, numTwo){
-    return numOne + numTwo;
+Calculator = function(){
+    this.value = 0;
 }
+
+Calculator.prototype.add = function(number){
+    if (typeof(number) == "number"){
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+}
+
+
+function whatCanIDrink(age){
+    if ( age < 0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!"
+    } else if ( age < 14 ){
+        return "Drink Toddy"
+    } else if ( age < 18 ){
+        return "Drink Coke"
+    } else if ( age < 21 ){
+        return "Drink Beer"
+    } else if ( age < 130 ){
+        return "Drink Whisky"
+    } else {
+        return "Sorry. I can’t tell what drink because that age is incorrect!"
+    }
+} 
